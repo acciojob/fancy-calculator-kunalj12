ee40eeb319acbabedca2b6022e80796cabbe2c91
kunal jangle <108586012+kunalj12@users.noreply.github.com>
2023-04-17 13:59:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+ let firstVariable = prompt("Enter the first variable:");
+      let result = (firstVariable * 2) + Number(prompt("Enter the second variable:"));
+      alert("The final result is: " + result);
